80578b3c1cf61c7ddc3cf2adb3e8ad8d8c58b151
Alexander Imperio <alexander.imperio@agetec.campogrande.ms.gov.br>
2021-05-12 00:32:14 -0400

    initial commit

---
+++ src/jogoGourmet/Dialogo.java
@@ -0,0 +1,12 @@
+package jogoGourmet;
+import javax.swing.JOptionPane;
+
+
+public class Dialogo {
+
+	public static void main(String[] args) {
+		JOptionPane.showConfirmDialog(null, "teste");
+
+	}
+
+}
